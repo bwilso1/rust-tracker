d313956c36b92a6d5faed0c3752197a579c656c5
B Wilson <bwilso1@umbc.edu>
2018-11-22 03:06:07 -0500

    finished automated queries, and merging of data into one JSON file

--- tracker.py
+++ tracker.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from time import sleep
 from item import *
 import json
+from manager import DataFileManager,Holder
 
 #NOTES:
 #		timeout seems to be 60 seconds, server side
@@ -11,28 +12,55 @@ import json
 #		args for link 'query', offset/page, items-per-page
 
 
-STEAM_URL = "https://steamcommunity.com/market/search?appid=252490&q="
-QUERYS = ['unknown+territory+hoodie','mp5']
-SKIN_EARN_LINK = "https://skinearn.com/api/shop?orderBy=price&order=DESC&game_id=5&name=%s&offset=%s&limit=%s"
-local = False
+STEAM_BASE_URL = "https://steamcommunity.com/market/search?appid=252490&q="
 
+
+SKIN_EARN_LINK = "https://skinearn.com/api/shop?orderBy=price&order=DESC&game_id=5&name=%s&offset=%s&limit=%s"
+
+STEAM_URLS = {'armor' : 'https://steamcommunity.com/market/search?category_252490_itemclass%5B%5D=any&category_252490_steamcat%5B%5D=tag_steamcat.armor&appid=252490&q=',
+		    'weapon' : 'https://steamcommunity.com/market/search?category_252490_itemclass%5B%5D=any&category_252490_steamcat%5B%5D=tag_steamcat.weapon&appid=252490&q=',
+		    'clothing' : 'https://steamcommunity.com/market/search?category_252490_itemclass%5B%5D=any&category_252490_steamcat%5B%5D=tag_steamcat.clothing&appid=252490&q='}
+
+STEAM_QUERIES = [['armor','carbon-metalhunter-army-stainless+NOT+armored+NOT+Plate'],
+			  ['armor','plate-press+NOT+engineer+NOT+conquistador+NOT+machina+NOT+looter+NOT+racing+NOT+hazard+NOT+wander+NOT+Space'],
+			  ['weapon','mp5-pearl+NOT+wild+NOT+dead+NOT+wasp+NOT+spitfire+NOT+nomad']
+			  ]
+WAIT_TIME = 65
+
+SKIN_EARN_QUERIES = ['mp5', 'pearl+python','face','metalhunter','christmas lights','press']
+
+OUTFILE = 'stats.json'
 def launch():
 
+	one_run_holder = Holder()
+	for tmp in STEAM_QUERIES:
+		query = STEAM_URLS[tmp[0]] + tmp[1]
+		print("steam query: %s" % (query,))
+		results = getSteamItems(query)
+		for item in results:
+			one_run_holder.addDollars(item.name, item.price)
+		
+		countDown(WAIT_TIME)
 	
-	print("getting steam items\n")
-	results = getSteamItems(QUERYS[1])
-	for x in results:
-		print(x)
+	for tmp in SKIN_EARN_QUERIES:
+		query = SKIN_EARN_LINK % (tmp,0,36)
+		print("skin earn query: %s" % (query,))
+		results = getSkinEarnItems(query)
+		for item in results:
+			one_run_holder.addCoins(item.name,item.price)
+		countDown(15)
+		
+	tmp = one_run_holder.dumpDict()
+	keys = tmp.keys()
+	manager = DataFileManager(OUTFILE)
+	for key in keys:
+		manager.add(key,tmp[key]['dollars'],tmp[key]['coins'])
+		
+	manager.dump(OUTFILE)
 		
 	
-	results = getSkinEarnItems(QUERYS[1])
-	print("\n\ngetting skin earn items\n")
-	for x in results:
-		print(x)
-		
-	print("sleeping for 60 seconds...\n")
-	countDown(60)
-	print("done")
+	
+	
 
 
 def removeUnicode(text):
@@ -43,6 +71,7 @@ def removeUnicode(text):
 	return asciiText
 
 def countDown(start_time=60):
+	print("sleeping for %s seconds" % start_time)
 	for x in range(start_time,0,-1):
 		print(x,end='',flush=True)
 		sleep(1)
@@ -60,7 +89,7 @@ def breakdown(element):
 	return item(name,quant,price)
 
 def getSkinEarnItems(query):
-	page = requests.get(SKIN_EARN_LINK % (query,0,36))
+	page = requests.get(query)
 	obj = json.loads(page.text)
 	results = []
 	for elm in obj['items']:
@@ -74,7 +103,7 @@ def getSteamItems(query):
 	#file = open('sample2.htm',encoding='utf-8')
 	#soup = BeautifulSoup(file)
 	
-	page = requests.get(STEAM_URL + query)
+	page = requests.get(query)
 	soup = BeautifulSoup(page.text,'html5lib')
 	steam_rows = soup.find_all('a',{'class': "market_listing_row_link"})
 	
@@ -82,14 +111,11 @@ def getSteamItems(query):
 	for div in steam_rows:
 		results.append(breakdown(div))
 	return results
-	#masks
-	#https://steamcommunity.com/market/search?category_252490_itemclass%5B%5D=any&category_252490_steamcat%5B%5D=tag_steamcat.armor&appid=252490&q=carbon-metalhunter-army-stainless+NOT+armored+NOT+Plate
-	
-	#chestplates
-	#https://steamcommunity.com/market/search?category_252490_itemclass%5B%5D=any&category_252490_steamcat%5B%5D=tag_steamcat.armor&appid=252490&q=plate-press+NOT+engineer+NOT+conquistador+NOT+machina+NOT+looter+NOT+racing+NOT+hazard+NOT+wander+NOT+Space
+
+	#plate-press+NOT+engineer+NOT+conquistador+NOT+machina+NOT+looter+NOT+racing+NOT+hazard+NOT+wander+NOT+Space
 	
 	#weapons
-	#https://steamcommunity.com/market/search?category_252490_itemclass%5B%5D=any&appid=252490&q=mp5-pearl+NOT+wild+NOT+dead+NOT+wasp+NOT+spitfire+NOT+nomad
+	#mp5-pearl+NOT+wild+NOT+dead+NOT+wasp+NOT+spitfire+NOT+nomad
 	
 	
 if __name__ == "__main__":
